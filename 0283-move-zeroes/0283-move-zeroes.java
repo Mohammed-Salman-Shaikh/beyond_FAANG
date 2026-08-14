070e4d43e2662ef115b1fb649873990191b37769
Mohammed Salman Shaikh <69892372+Mohammed-Salman-Shaikh@users.noreply.github.com>
2026-08-14 23:56:58 +0530

    Time: 2 ms (91.97%), Space: 47.8 MB (57.69%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // Pointer to track where the next non-zero element should be placed
         int insertPos = 0;
 
-        // Step 1: Shift non-zero elements forward
-        for (int num : nums) {
-            if (num != 0) {
+        for(int num: nums){
+            if(num!=0){
                 nums[insertPos] = num;
                 insertPos++;
             }
         }
 
-        // Step 2: Fill the remaining positions with zeros
-        while (insertPos < nums.length) {
+        while(insertPos < nums.length){
             nums[insertPos] = 0;
             insertPos++;
         }
